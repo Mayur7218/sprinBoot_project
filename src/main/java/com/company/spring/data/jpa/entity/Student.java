401401a9b7037522eb914b212c549c12274618de
Mjadhav7218 <mjadhav@truxnow.com>
2025-02-11 16:21:30 +0530

    created entity for Student and embeddable Guardian

--- src/main/java/com/company/spring/data/jpa/entity/Student.java
+++ src/main/java/com/company/spring/data/jpa/entity/Student.java
@@ -1,6 +1,14 @@
 package com.company.spring.data.jpa.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,13 +19,35 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Table(
+        name = "tbl_student",
+        uniqueConstraints = @UniqueConstraint(
+        name = "emailid_unique",
+        columnNames = "email_address"
+        )
+)
 public class Student {
 
+    @Id  //primary key use
+    @SequenceGenerator(
+            name = "student_sequence",
+            sequenceName = "student_sequence",
+            allocationSize = 1
+    )
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator= "student_sequence"
+    )
     private Long studentId;
     private String firstName;
     private String lastName;
+
+    @Column(
+            name = "email_address",
+            nullable = false
+    )
     private String emailId;
-    private String guardianName;
-    private String guardianEmail;
-    private String guardianMobile;
+
+    @Embedded
+    public Guardian guardian;
 }
